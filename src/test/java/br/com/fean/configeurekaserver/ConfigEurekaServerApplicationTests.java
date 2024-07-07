7fb5b4fbebe51f1d450a8497a8d9ddf7f2adbbad
Francisco Nascimento <f.b.nascimento@avanade.com>
2024-07-07 01:41:11 -0300

    Adicionando estrutura geral do Saga Commit de migração de estação de trabalho

---
+++ src/test/java/br/com/fean/configeurekaserver/ConfigEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fean.configeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
